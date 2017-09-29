2e4c66c2b92879812db132cf58ad03edb6328495
Ryan Shin <changyeolshin@gmail.com>
2017-09-29 12:33:50 -0700

    Added the start feature. #3

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ class Car {
   }
 
   accelerate(amount) {
-    this.speed = amount
+    this.speed += amount
   }
 
   move() {
@@ -39,20 +39,41 @@ function renderCar(carSprite) {
   return $car
 }
 
-var $createButton = document.querySelector('#create')
+const $createButton = document.querySelector('#create')
 $createButton.addEventListener('click', function () {
-  const newCar = new Car('EAST', 50, [500, 500])
   const carSprite = 'images/car-sprite.png'
   const $car = renderCar(carSprite)
   document.querySelector('.game').appendChild($car)
   $createButton.setAttribute('class', 'hidden')
 })
 
-var $clearButton = document.querySelector('#clear')
+const $clearButton = document.querySelector('#clear')
 $clearButton.addEventListener('click', function () {
   if (document.querySelector('.car')) {
     const $car = document.querySelector('.car')
     document.querySelector('.game').removeChild($car)
     $createButton.setAttribute('class', 'button')
+  }
+})
+
+document.body.addEventListener('keypress', function (e) {
+  switch (e.key) {
+    case 'Enter':
+      if (document.querySelector('img')) {
+        const newCar = new Car('EAST', 1, [500, 500])
+        const $car = document.querySelector('img')
+        let { top, left } = $car.style
+        setInterval(function () {
+          newCar.move()
+          Object.assign($car.style, {
+            left: `${newCar.location[0]}px`,
+            top: `${newCar.location[1]}px`
+          })
+          console.log(left, top)
+        }, 16)
+      }
+      break
+    default:
+
   }
 })
