245ca60202739fa69825faeecf6a7ab9d6d90cc8
Ryan Shin <changyeolshin@gmail.com>
2017-09-30 13:40:58 -0700

    Added in the acceleration feature. #11

--- main.js
+++ main.js
@@ -42,11 +42,11 @@ function renderCar(carSprite) {
 
 function moveCar(newCar, $car) {
   const moveCar = setInterval(function () {
-    if (newCar.location[0] === 900 || newCar.location[1] === 900 || newCar.location[0] === 0 || newCar.location[1] === 0) {
+    newCar.move()
+    if (newCar.location[0] >= 900 || newCar.location[1] >= 900 || newCar.location[0] <= 0 || newCar.location[1] <= 0) {
       const $seconds = document.querySelector('.seconds')
       gameOver($car, newCar.id, timer, $seconds)
     }
-    newCar.move()
     Object.assign($car.style, {
       left: `${newCar.location[0]}px`,
       bottom: `${newCar.location[1]}px`
@@ -55,11 +55,12 @@ function moveCar(newCar, $car) {
   return moveCar
 }
 
-function startTimer(timer, $seconds) {
+function startTimer(newCar, timer, $seconds) {
   if (!timer.id) {
     timer.id = setInterval(function () {
       timer.seconds++
       $seconds.textContent = timer.seconds
+      checkTimer(newCar, timer)
     }, 1000)
     return timer.id
   }
@@ -67,15 +68,18 @@ function startTimer(timer, $seconds) {
 
 function clearTimer(timer, $seconds) {
   clearInterval(timer.id)
+  timer.id = null
   timer.seconds = 0
   $seconds.textContent = timer.seconds
 }
 
 function gameOver($car, id, timer, $seconds) {
   stopCar(id)
-  document.querySelector('.game').removeChild($car)
   newCar = null
-  $createButton.setAttribute('class', 'button')
+  document.querySelector('.game').removeChild($car)
+  const $gameover = renderGameOver()
+  document.querySelector('.container').setAttribute('class', 'hidden')
+  document.body.appendChild($gameover)
   clearTimer(timer, $seconds)
 }
 
@@ -103,10 +107,49 @@ function turnRight($car, car) {
   car.turn('EAST')
 }
 
+function checkTimer(car, timer) {
+  if (timer.seconds % 10 === 0) {
+    car.accelerate(car.speed)
+  }
+}
+
+function renderGameOver() {
+  const $gameover = document.createElement('div')
+  $gameover.setAttribute('class', 'gameover-screen')
+
+  const $gameoverHeader = document.createElement('h1')
+  $gameoverHeader.setAttribute('class', 'gameover-header')
+  $gameoverHeader.textContent = 'GAME OVER'
+  $gameover.appendChild($gameoverHeader)
+
+  const $result = document.createElement('p')
+  $result.setAttribute('class', 'result')
+  $result.textContent = `Your time was ${timer.seconds} second(s).`
+  $gameover.appendChild($result)
+
+  Object.assign(document.body.style, {
+    'background-color': 'black'
+  })
+
+  const $continue = renderContinueButton()
+  $gameover.appendChild($continue)
+
+  return $gameover
+}
+
+function renderContinueButton() {
+  const $continue = document.createElement('button')
+  $continue.setAttribute('class', 'button')
+  $continue.setAttribute('id', 'continue')
+  $continue.textContent = 'CONTINUE?'
+
+  return $continue
+}
+
 let newCar = null
 const $createButton = document.querySelector('#create')
 $createButton.addEventListener('click', function () {
-  newCar = new Car(null, 'EAST', 10, [500, 500])
+  newCar = new Car(null, 'EAST', 2, [500, 500])
   const carSprite = 'images/car-sprite.png'
   const $car = renderCar(carSprite)
   document.querySelector('.game').appendChild($car)
@@ -117,8 +160,24 @@ const $clearButton = document.querySelector('#clear')
 $clearButton.addEventListener('click', function () {
   if (document.querySelector('.car')) {
     const $car = document.querySelector('.car')
+    let newCar = null
     document.querySelector('.game').removeChild($car)
-    $createButton.setAttribute('class', 'button')
+  }
+  let newCar = null
+  $createButton.setAttribute('class', 'button')
+})
+
+document.body.addEventListener('click', function (e) {
+  if (e.target.getAttribute('id') === 'continue') {
+    const $gameover = document.querySelector('.gameover-screen')
+    $gameover.setAttribute('class', 'hidden')
+
+    Object.assign(document.body.style, {
+      'background-color': '#f4f5f4'
+    })
+
+    const $container = document.querySelector('.hidden')
+    $container.setAttribute('class', 'container')
   }
 })
 
@@ -130,30 +189,40 @@ document.body.addEventListener('keypress', function (e) {
   const $car = document.querySelector('.car')
   switch (e.key) {
     case 'Enter':
-      if (document.querySelector('img')) {
-        const $car = document.querySelector('img')
+      if (document.querySelector('.car')) {
+        const $car = document.querySelector('.car')
         if (!newCar.id) {
           newCar.id = moveCar(newCar, $car)
-          timer.id = startTimer(timer, document.querySelector('.seconds'))
+          timer.id = startTimer(newCar, timer, document.querySelector('.seconds'))
         }
       }
       break
     case 'b':
-      stopCar(newCar.id)
-      clearInterval(timer.id)
-      newCar.id = null
-      timer.id = null
+      if ($car) {
+        stopCar(newCar.id)
+        clearInterval(timer.id)
+        newCar.id = null
+        timer.id = null
+      }
       break
     case 'w':
-      turnUp($car, newCar)
+      if ($car) {
+        turnUp($car, newCar)
+      }
       break
     case 'a':
-      turnLeft($car, newCar)
+      if ($car) {
+        turnLeft($car, newCar)
+      }
       break
     case 's':
-      turnDown($car, newCar)
+      if ($car) {
+        turnDown($car, newCar)
+      }
       break
     case 'd':
-      turnRight($car, newCar)
+      if ($car) {
+        turnRight($car, newCar)
+      }
   }
 })
